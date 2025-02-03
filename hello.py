f0ba62c0cc561d2950e138ae2030ecd6e8d3534f
immmauel Ndaule <immanuelndaule@gmail.com>
2025-02-03 14:00:06 +0200

    initial commit

---
+++ hello.py
@@ -0,0 +1,21 @@
+# print hello world on the screen
+import os
+os.system("clear")
+
+full_name = "Immanuel Ndaule"
+
+age = 41
+names=["john","mary","bob"]
+
+fav_pizza= {"john": "pepperroni",
+           "mary": "mushroom",
+             "bob": "cheeese"}
+
+print(fav_pizza["mary"])
+
+
+
+
+
+
+
